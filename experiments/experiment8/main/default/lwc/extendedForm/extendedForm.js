7b3fa9039ae054af17647d6ceb84794f20c71fd8
Stephan H. Wissel <stw@linux.com>
2019-04-05 22:13:27 +0800

    added eventBubble

--- experiments/experiment8/main/default/lwc/extendedForm/extendedForm.js
+++ experiments/experiment8/main/default/lwc/extendedForm/extendedForm.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-debugger */
 import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ExtendedForm extends LightningElement {
     @api recordTypeId;
@@ -9,5 +11,55 @@ export default class ExtendedForm extends LightningElement {
         super();
         // Holds our fields
         this.specialFieldMap = {};
+    }
+
+    connectedCallback() {
+        const slot = this.template.querySelector('slot');
+        if (slot) {
+            slot.addEventListener('valueChanged', this.listenDataChange, true);
+        }
+    }
+
+    /* Capturing all changed values from our data control */
+    listenDataChange = event => {
+        debugger;
+        let fieldInfo = event.detail;
+        this.specialFieldMap[fieldInfo.name] = fieldInfo.value;
+    };
+
+    formLoadHandler() {
+        debugger;
+        let tangoEvent = new CustomEvent('tango', {
+            details: { color: 'red', mood: 'good' }
+        });
+        this.dispatchEvent(tangoEvent);
+    }
+
+    formSubmitHandler = event => {
+        event.preventDefault();
+        const fields = event.detail.fields;
+        for (let key in this.specialFieldMap) {
+            if (this.specialFieldMap.hasOwnProperty(key)) {
+                fields[key] = this.specialFieldMap[key];
+            }
+        }
+
+        this.template
+            .querySelector('lightning-record-edit-form')
+            .submit(fields);
+    };
+
+    formSuccessHandler(event) {
+        const record = event.detail;
+        debugger;
+        const evt = new ShowToastEvent({
+            title: 'Record created',
+            message:
+                this.objectApiName +
+                ' created: ' +
+                (record.fields.Name.displayValue || record.fields.Name.value),
+            variant: 'success'
+        });
+        this.dispatchEvent(evt);
     }
 }
--- experiments/experiment8/main/default/lwc/specialInput/specialInput.js
+++ experiments/experiment8/main/default/lwc/specialInput/specialInput.js
@@ -1,3 +1,34 @@
-import { LightningElement } from 'lwc';
+/* eslint-disable no-debugger */
+/* eslint-disable no-console */
+import { LightningElement, api } from 'lwc';
 
-export default class SpecialInput extends LightningElement {}
+export default class SpecialInput extends LightningElement {
+    @api extendedFieldName;
+    @api value = '';
+
+    handleChange = event => {
+        event.stopPropagation();
+        window.clearTimeout(this.timeout);
+        let realData = {
+            name: this.extendedFieldName,
+            value: 'X:' + event.target.value
+        };
+        // Do we need this
+        this.value = realData.value;
+        this.fireChange(realData);
+    };
+
+    fireChange(changedValue) {
+        let customChange = new CustomEvent('valueChanged', {
+            detail: changedValue,
+            bubbles: true,
+            cancelable: true
+        });
+        this.dispatchEvent(customChange);
+    }
+
+    handleTango(event) {
+        debugger;
+        console.log(event);
+    }
+}
