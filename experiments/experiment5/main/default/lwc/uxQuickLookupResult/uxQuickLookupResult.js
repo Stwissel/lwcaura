149c17b5022e9f16922dc8d9433b401c63bae0d5
Stephan H. Wissel <stw@linux.com>
2019-04-06 14:26:07 +0800

    Added field display

--- experiments/experiment5/main/default/lwc/uxQuickLookupResult/uxQuickLookupResult.js
+++ experiments/experiment5/main/default/lwc/uxQuickLookupResult/uxQuickLookupResult.js
@@ -4,11 +4,32 @@ export default class UxQuickLookupResult extends LightningElement {
     @api iconName;
     @api record;
 
-    handleOnClick(event) {
-        let Id = event.target.getAttribute('data-id');
-        let Name = event.target.innerText;
-        let payload = { detail: { Id: Id, Name: Name } };
+    handleOnClick = () => {
+        let Id = this.record.Id;
+        let Name = this.record.Name;
+        let payload = {
+            detail: { Id: Id, Name: Name }
+        };
         let selection = new CustomEvent('selection', payload);
         this.dispatchEvent(selection);
+    };
+
+    get fieldNameResults() {
+        if (!this.record) {
+            return null;
+        }
+        let foundSomething = false;
+        let result = [];
+        for (let fName in this.record) {
+            if (
+                fName !== 'Id' &&
+                fName !== 'Name' &&
+                this.record.hasOwnProperty(fName)
+            ) {
+                result.push({ name: fName, value: this.record[fName] });
+                foundSomething = true;
+            }
+        }
+        return foundSomething ? result : null;
     }
 }
